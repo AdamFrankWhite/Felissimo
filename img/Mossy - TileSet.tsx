35add4d4af528c0bace983023dcece40be4b2d5e
Adam White <adamfrankwhite@gmail.com>
2022-06-04 22:56:17 +0100

    remove sprite folder

--- img/Mossy - TileSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Mossy - TileSet" tilewidth="12" tileheight="12" tilecount="88804" columns="298">
- <image source="Mossy Assets/Mossy Tileset/Mossy - TileSet.png" width="3584" height="3584"/>
-</tileset>
